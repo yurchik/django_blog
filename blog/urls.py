b6d5916992c3cacd0c930f8dd43ef7c5cfeb528a
yurchik <15_yurchik@ukr.net>
2016-11-07 08:17:04 +0200

    Add detail view with post

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url, include
 from django.views.generic import DetailView, ListView
 from .models import Post
 
+app_name = 'blog'
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name='blog/blog.html'))
-    ]
+    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name='blog/blog.html')),
+    url(r'(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name='blog/post.html'), name='post')
+]
